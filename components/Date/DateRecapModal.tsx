f2357fc0101f4958d58dc2ccf57ca71860d922a9
wodenp98 <paulvigneron4698@gmail.com>
2024-10-30 17:46:20 +0100

    add useeffect

--- components/Date/DateRecapModal.tsx
+++ components/Date/DateRecapModal.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Dialog,
   DialogContent,
