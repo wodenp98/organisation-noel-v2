e55afd584d97ea7884de97d64a8a69a68a321f48
wodenp98 <paulvigneron4698@gmail.com>
2024-11-03 22:03:10 +0100

    fix build

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 import { UserAuthForm } from "@/components/user-auth-form";
-import { auth } from "@/lib/auth";
+import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 export default async function Page() {
   const session = await auth();
